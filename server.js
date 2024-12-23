9ad367261f46da7ea865f34c40da081e206755ef
Muhamad Daffa Al Ghifary <daffa.alghifary62@gmail.com>
2024-12-23 11:37:31 +0700

    server

--- server.js
+++ server.js
@@ -2,10 +2,31 @@ const http = require('http');
 
 const server = http.createServer(function(request, response) {
   response.statusCode = 200;
-  response.setHeader('Content-Type', 'text/plain');
-  response.end('Hello World');
+  response.setHeader('Content-Type', 'text/html');
+  response.end(HTML);
 });
 
 server.listen({ port: 3000, host: 'localhost' }, function() {
   console.log('Server is running!');
 });
+
+const HTML = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>My Hello World</title>
+    <style>
+      body {
+        background-color: black;
+        color: yellow;
+        text-align: center;
+        font-size: 40px;
+      }
+    </style>
+  </head>
+  <body>
+    Hello World
+  </body>
+</html>
+`;
