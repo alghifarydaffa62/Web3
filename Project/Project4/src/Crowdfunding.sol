0bfc49e3b952afc53f9939eefd6cbfbd3a780136
Muhammad Daffa Al Ghifary <daffa.alghifary62@gmail.com>
2025-01-26 17:36:01 +0700

    Crowdfunding program done

--- Project/Project4/src/Crowdfunding.sol
+++ Project/Project4/src/Crowdfunding.sol
@@ -8,6 +8,7 @@ contract CrowdFunding {
         uint deadline;
         uint totalAmount;
         bool isComplete;
+        mapping(address => uint) donates;
     }
 
     mapping(uint => Campaign) public campaigns;
@@ -15,16 +16,17 @@ contract CrowdFunding {
 
     event CampaignCreated(address indexed owner, uint target, uint deadline);
     event donateSuccess(address indexed donor, uint amount);
+    event withdrawn(address indexed owner, uint amount);
+    event refunded(address indexed donor, uint amount);
 
     function createCampaign(uint _targetAmount, uint _deadline) external {
         campaignID++;
-        campaigns[campaignID] = Campaign({
-            owner: msg.sender,
-            targetAmount: _targetAmount,
-            totalAmount: 0,
-            deadline: block.timestamp + _deadline,
-            isComplete: false
-        });
+        Campaign storage campaign = campaigns[campaignID];
+        campaign.owner = msg.sender;
+        campaign.targetAmount = _targetAmount;
+        campaign.deadline = block.timestamp + _deadline;
+        campaign.totalAmount = 0;
+        campaign.isComplete = false;
 
         emit CampaignCreated(msg.sender, _targetAmount, _deadline);
     }
@@ -36,13 +38,40 @@ contract CrowdFunding {
         require(msg.value > 0, "must send ether!");
 
         campaign.totalAmount += msg.value;
+        campaign.donates[msg.sender] += msg.value;
         emit donateSuccess(msg.sender, msg.value);
     }
 
     function withdraw(uint Id) external payable {
         Campaign storage campaign = campaigns[Id];
-        require(campaign.totalAmount == campaign.targetAmount);
+        require(msg.sender == campaign.owner, "You are not the owner");
+        require(campaign.totalAmount >= campaign.targetAmount, "Target has not been achieved");
         require(block.timestamp > campaign.deadline, "Campaign is still active!");
-        require(!campaign.isComplete);
+        require(!campaign.isComplete, "Funds already withdrawn!");
+
+        campaign.isComplete = true;
+        uint total = campaign.totalAmount;
+    
+        (bool success,) = msg.sender.call{value: total}("");
+        require(success);
+        campaign.totalAmount = 0;
+
+        emit withdrawn(msg.sender, campaign.targetAmount);
+    }
+
+    function refund(uint Id) external payable {
+        Campaign storage campaign = campaigns[Id];
+        require(campaign.totalAmount < campaign.targetAmount, "Target is achieved! no refunds!");
+        require(block.timestamp > campaign.deadline, "Campaign is still active!, no refunds!");
+        require(!campaign.isComplete, "funds is withdrawn, no refunds!");
+
+        uint donateAmount = campaign.donates[msg.sender];
+        require(donateAmount > 0, "No donations, no refunds!");
+
+        (bool success,) = msg.sender.call{value: donateAmount}("");
+        require(success);
+        campaign.donates[msg.sender] = 0;
+
+        emit refunded(msg.sender, campaign.donates[msg.sender]);
     }
 }
