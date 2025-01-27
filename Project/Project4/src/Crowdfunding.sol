4c7016c46565f8faae4c1310cd443de5720a7e9a
Muhammad Daffa Al Ghifary <daffa.alghifary62@gmail.com>
2025-01-27 10:48:56 +0700

    add uniqueID

--- Project/Project4/src/Crowdfunding.sol
+++ Project/Project4/src/Crowdfunding.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.20;
 contract CrowdFunding {
     struct Campaign {
         address owner;
+        uint ID;
         uint targetAmount;
         uint deadline;
         uint totalAmount;
@@ -11,18 +12,25 @@ contract CrowdFunding {
         mapping(address => uint) donates;
     }
 
-    mapping(uint => Campaign) public campaigns;
-    uint campaignID;
+    mapping(uint => Campaign) private campaigns;
+    uint campaignCount;
 
     event CampaignCreated(address indexed owner, uint target, uint deadline);
     event donateSuccess(address indexed donor, uint amount);
     event withdrawn(address indexed owner, uint amount);
     event refunded(address indexed donor, uint amount);
 
+    function generateId() internal view returns(uint) {
+        uint hash = uint(keccak256(abi.encodePacked(msg.sender, block.timestamp, campaignCount)));
+        return hash % 100000000;
+    }
+
     function createCampaign(uint _targetAmount, uint _deadline) external {
-        campaignID++;
-        Campaign storage campaign = campaigns[campaignID];
+        campaignCount++;
+        uint uniqueID = generateId();
+        Campaign storage campaign = campaigns[uniqueID];
         campaign.owner = msg.sender;
+        campaign.ID = uniqueID;
         campaign.targetAmount = _targetAmount;
         campaign.deadline = block.timestamp + _deadline;
         campaign.totalAmount = 0;
@@ -33,6 +41,7 @@ contract CrowdFunding {
 
     function donate(uint Id) external payable {
         Campaign storage campaign = campaigns[Id];
+        require(campaign.owner != address(0), "Campaign is not existed!");
         require(!campaign.isComplete, "Campaign already closed!");
         require(block.timestamp <= campaign.deadline, "Campaign already expired!");
         require(msg.value > 0, "must send ether!");
@@ -44,24 +53,26 @@ contract CrowdFunding {
 
     function withdraw(uint Id) external payable {
         Campaign storage campaign = campaigns[Id];
+        require(campaign.owner != address(0), "Campaign is not existed!");
         require(msg.sender == campaign.owner, "You are not the owner");
         require(campaign.totalAmount >= campaign.targetAmount, "Target has not been achieved");
         require(block.timestamp > campaign.deadline, "Campaign is still active!");
         require(!campaign.isComplete, "Funds already withdrawn!");
 
-        campaign.isComplete = true;
         uint total = campaign.totalAmount;
     
         (bool success,) = msg.sender.call{value: total}("");
         require(success);
         campaign.totalAmount = 0;
+        campaign.isComplete = true;
 
         emit withdrawn(msg.sender, campaign.targetAmount);
     }
 
     function refund(uint Id) external payable {
         Campaign storage campaign = campaigns[Id];
-        require(campaign.totalAmount < campaign.targetAmount, "Target is achieved! no refunds!");
+        require(campaign.owner != address(0), "Campaign is not existed!");
+        require(campaign.totalAmount < campaign.targetAmount, "Target is reached!, no refunds!");
         require(block.timestamp > campaign.deadline, "Campaign is still active!, no refunds!");
         require(!campaign.isComplete, "funds is withdrawn, no refunds!");
 
