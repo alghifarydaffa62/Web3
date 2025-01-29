1e4440b35051f472922dfdf5b28ff453808e4d01
Muhammad Daffa Al Ghifary <daffa.alghifary62@gmail.com>
2025-01-29 20:05:56 +0700

    Escrow solidity

--- Project/Escrow/src/Escrow.sol
+++ Project/Escrow/src/Escrow.sol
@@ -5,22 +5,51 @@ contract Escrow {
     address public deployer;
     address public services;
     address public arbiter;
-    uint money;
+    uint public balance;
 
     constructor(address _services, address _arbiter) {
         deployer = msg.sender;
         services = _services;
         arbiter = _arbiter;
     }
-    
-    event transferSuccess(address indexed deployer, uint amount);
 
-    function Transfer() external payable {
+    modifier onlyArbiter {
+        require(msg.sender == arbiter, "You are not the arbiter!");
+        _;
+    }
+    
+    event depositSuccess(address indexed deployer, uint amount);
+    event servicesPayed(address indexed arbiter, address indexed service, uint amount);
+    event refundSuccess(address indexed arbiter, address indexed deployer, uint amount);
+
+    function deposit() external payable {
         require(msg.sender == deployer, "You are not the deployer!");
         require(msg.value > 0, "Must send ether!");
-        money += msg.value;
-        emit transferSuccess(msg.sender, msg.value);
+        balance += msg.value;
+
+        emit depositSuccess(msg.sender, msg.value);
     }
 
+    function approvePayment() external payable onlyArbiter {
+        require(balance > 0, "Deployer hasn't send ether!");
+
+        uint amount = balance;
+        (bool success,) = services.call{value: amount}("");
+        require(success, "Failed!");
+        balance = 0;
+
+        emit servicesPayed(msg.sender, services, amount);
+    }
+
+    function refund() external payable onlyArbiter {
+        require(balance > 0, "Deployer hasn't send ether!");
+
+        uint amount = balance;
+        (bool success,) = deployer.call{value: amount}("");
+        require(success, "refund failed!");
+        balance = 0;
+
+        emit refundSuccess(msg.sender, deployer, amount);
+    }
     
 }
