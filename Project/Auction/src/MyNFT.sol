3c24049850c11aed59b1cb05ca52bec3da99a4b2
Muhammad Daffa Al Ghifary <daffa.alghifary62@gmail.com>
2025-02-11 15:55:34 +0700

    auction

--- Project/Auction/src/MyNFT.sol
+++ Project/Auction/src/MyNFT.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "openzeppelin-contracts/contracts/access/Ownable.sol";
 
-abstract contract MyNFT is ERC721URIStorage, Ownable {
+contract MyNFT is ERC721URIStorage, Ownable {
     uint private _nextTokenId;
 
     constructor() ERC721("MyNFT", "MNFT") {}
