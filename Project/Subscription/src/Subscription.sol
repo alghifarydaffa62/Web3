b9336151d76ae6804a6a8a1a4213791f33a3ff53
Muhammad Daffa Al Ghifary <daffa.alghifary62@gmail.com>
2025-02-20 19:12:07 +0700

    Setup project

---
+++ Project/Subscription/src/Subscription.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+// import "@openzepplin/contracts/token/ERC20/IERC20.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Subscription {
+    
+}
