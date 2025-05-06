324261d4141589b2ac85f280b968d9b7b59af924
M Daffa A <daffa.alghifary62@gmail.com>
2025-05-06 18:52:58 +0700

    add: register function

--- Project/Marketplace/src/Marketplace.sol
+++ Project/Marketplace/src/Marketplace.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.20;
 
 contract Marketplace {
     struct Buyer {
@@ -22,8 +22,32 @@ contract Marketplace {
         uint revenue;
     }
 
+    struct Market {
+        uint[] activeProducts;
+    }
+
     uint public productCount;
     mapping(uint => Product) public products;
     mapping(address => Seller) public sellers;
     mapping(address => Buyer) public buyers;
+
+    function registerBuyer() external {
+        require(buyers[msg.sender].buyer == address(0), "Already registered");
+
+        buyers[msg.sender] = Buyer({
+            buyer: msg.sender,
+            cart: new uint[],
+            totalSpent: 0
+        });        
+    }
+
+    function registerSeller() external {
+        require(sellers[msg.sender].seller == address(0), "Already registered!");
+        
+        sellers[msg.sender] = Seller({
+            seller: msg.sender,
+            productOwn: new uint ,
+            revenue: 0
+        });
+    }
 }
