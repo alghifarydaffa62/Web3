aee737c1425d798481975540a363f5f3ad4cf8fd
M Daffa A <daffa.alghifary62@gmail.com>
2025-05-06 18:06:41 +0700

    setup marketplace

--- Project/Marketplace/src/Marketplace.sol
+++ Project/Marketplace/src/Marketplace.sol
@@ -2,5 +2,28 @@
 pragma solidity ^0.8.13;
 
 contract Marketplace {
+    struct Buyer {
+        address buyer;
+        uint[] cart;
+        uint totalSpent;
+    }
 
+    struct Product {
+        address owner;
+        string name;
+        uint id;
+        uint price;
+        uint stock;
+    }
+
+    struct Seller {
+        address seller;
+        uint[] productOwn;
+        uint revenue;
+    }
+
+    uint public productCount;
+    mapping(uint => Product) public products;
+    mapping(address => Seller) public sellers;
+    mapping(address => Buyer) public buyers;
 }
