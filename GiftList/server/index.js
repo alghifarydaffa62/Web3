8dc5d2c79853a173b4bddb4617decc51b3d6d6ea
M Daffa A <daffa.alghifary62@gmail.com>
2025-05-09 08:42:27 +0700

    week 2 assignment done

---
+++ GiftList/server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const verifyProof = require('../utils/verifyProof');
+const MerkleTree = require("../utils/MerkleTree");
+const niceList = require("../utils/niceList.json");
+
+const port = 1225;
+
+const app = express();
+app.use(express.json());
+
+const merkleTree = new MerkleTree(niceList); 
+const MERKLE_ROOT = merkleTree.getRoot();
+
+app.post('/gift', (req, res) => {
+  const { name, proof } = req.body; 
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
+  
+  if(isInTheList) {
+    res.send("You got a toy robot!");
+  }
+  else {
+    res.send("You are not on the list :(");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}!`);
+});
