65f292bf960acf3064e0f1346b964f79d807e7fe
M Daffa A <daffa.alghifary62@gmail.com>
2025-06-29 09:57:52 +0700

    documentation

--- Project/Payroll/src/Payroll.sol
+++ Project/Payroll/src/Payroll.sol
@@ -1,30 +1,73 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-
+/**
+ * @title Payroll smart contract
+ * @notice A Payroll smart contract to manage employees, pay salaries and remove employees
+ * @dev Only the admin (contract deployer) can manage payroll operations
+ */
 contract Payroll {
+    
+    // ----------------------------
+    // Structs and Variables
+    // -----------------------------
+
+    /**
+     * @dev Represents an employee with address and salary
+     */
     struct Employee {
         address employee;
         uint salary;
     }
     
+    /// @notice List of registered employees
     Employee[] public employees;
+
+    /// @notice Admin address
     address public Admin;
+
+    /// @notice Balance of the payroll contract
     uint companyBalance;
 
+    /// @dev Set the contract deployer as the admin
     constructor() {
         Admin = msg.sender;
     }
 
+    // -----------------------
+    // Modifier
+    // -----------------------
+    
+    /// @dev Restricts access to only the admin to call certain functions
     modifier onlyAdmin {
         require(msg.sender == Admin, "only Admin!");
         _;
     }
 
+    // ------------------------
+    // Events
+    // ------------------------
+
+    /// @notice Emitted when admin successfully deposit ether to contract
     event depositSuccess(address indexed admin, uint amount);
+
+    /// @notice Emitted when admin successfully registers an employee
     event employeeRegisterSuccess(address indexed admin, address indexed employee, uint salary);
+
+    /// @notice Emitted when admin successfully pay the employee salary
     event salaryPayed(address indexed admin, address indexed employee, uint salary);
+
+    /// @notice Emitted when admin successfully remove an employee
     event employeeFired(address indexed admin, address employee);
 
+    // -----------------------
+    // Core Functions
+    // -----------------------
+
+    /**
+     * @notice Allows the admin to deposit ether to contract balance
+     * @dev Deposit require value more than zero
+     * @dev Successfully deposit ether using this funtion will emit the despositSuccess event
+     */
     function deposit() external payable onlyAdmin {
         require(msg.value > 0, "Must send ether!");
         companyBalance += msg.value;
@@ -32,6 +75,15 @@ contract Payroll {
         emit depositSuccess(msg.sender, msg.value);
     }
 
+    /**
+     * @notice Allows the admin to add new employee and their the salary
+     * @param _employee The address of the new employee
+     * @param _salary The amount of salary
+     * @dev Only unregistered employee can be added using this 
+        function and the salary must be greater than zero
+     * @dev Successfully added new mployee using this function
+         will emit the EmployeeRegisterSuccess event
+     */
     function addEmployee(address _employee, uint _salary) external onlyAdmin {
         require(!findEmployee(_employee), "Employee already registered!");
         require(_salary > 0, "Salary can't be zero!");
